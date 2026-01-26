43a787cfab7c85dab5d03c2d90af961dc96e1364
PolRodriguez-CTS <prodriguez@ctsformacion.com>
2026-01-26 11:00:20 +0100

    26/01

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Rigidbody _rb;
+
+    private float _shootForce = 10f;
+
+    void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        _rb.AddForce(transform.forward * _shootForce, ForceMode.Impulse);
+    }
+
+    
+    void OnTriggerEnter(Collider other)
+    {
+        gameObject.SetActive(false);  
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    //Inputs
+    private InputAction _shootAction;
+
+    [Header("Shoot")]
+    [SerializeField] private Transform _bulletTransform;
+    [SerializeField] private GameObject _bulletPrefab;
+
+    void Awake()
+    {
+        _shootAction = InputSystem.actions["Attack"];
+    }
+
+    void Start()
+    {}
+
+    void Update()
+    {
+        if(_shootAction.WasPressedThisFrame())
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        //Instantiate(_bulletPrefab, _bulletTransform.position, _bulletTransform.rotation);
+
+        GameObject bullet = PoolManager.Instance.GetPooledObject("Bullets", _bulletTransform.position, _bulletTransform.rotation);
+        bullet.SetActive(true);
+    }
+}
